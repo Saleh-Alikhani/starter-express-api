e2b18cc2deede0c732fb6656eef4d89f79bf6187
Saleh Alikhani <salehalikhani@gmail.com>
2023-12-28 16:32:20 +0330

    test

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const { writeFileSync, readFileSync } = require('fs');
+
+const app = express();
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+  console.log('Just got a request!');
+  writeFileSync('test.txt', 'req');
+
+  res.send('Yo!');
+});
+
+app.get('/get', (req, res) => {
+  const data = readFileSync('test.txt');
+  res.send(data);
+});
+
+app.listen(process.env.PORT || 3000);
