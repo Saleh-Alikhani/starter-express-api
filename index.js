12c9792edd1cb09ea45dc10ded241875476c9bd5
Saleh Alikhani <salehalikhani@gmail.com>
2024-02-18 15:05:49 +0330

    tst

--- index.js
+++ index.js
@@ -200,6 +200,7 @@ app.get('/tel', async (req, res) => {
     const subs = [];
     for (let i of result) {
       if (!subs.includes(i[0])) {
+        console.log(i[0])
         subs.push(i[0]);
       }
     }
